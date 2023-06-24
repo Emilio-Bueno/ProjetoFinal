5e868ccf2065bdbb54712fdb28e472904107e022
emilio--bueno <emilio__bueno@outlook.com>
2023-06-23 22:59:47 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunosFormComponent } from './alunos-form/alunos-form.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { ProfessoresFormComponent } from './professores-form/professores-form.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    AlunosFormComponent,
+    ProfessoresComponent,
+    ProfessoresFormComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
