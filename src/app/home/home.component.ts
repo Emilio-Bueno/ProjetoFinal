02085f7d72985227447bf925ba0c667104f611be
emilio--bueno <emilio__bueno@outlook.com>
2023-06-24 00:32:50 -0300

    Tela Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Alunos } from '../aluno';
+import { AlunoService } from '../aluno.service';
+import { Professores } from '../professor';
+import { ProfessorService } from '../professor.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  alunos: Alunos[] = [];
+  professores: Professores[] = [];
 
+  constructor(private alunoService: AlunoService, private professorService: ProfessorService) { }
+
+  ngOnInit(): void {
+    this.loadAlunos();
+    this.loadProfessores();
+  }
+
+  loadAlunos() {
+    this.alunoService.getAlunos().subscribe({
+      next: data => this.alunos = data
+    });
+  }
+
+  loadProfessores() {
+    this.professorService.getProfessor().subscribe({
+      next: data => this.professores = data
+    });
+  }
 }
